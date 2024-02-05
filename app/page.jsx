0dfd09e96a140a3808dac072715f813b2f632452
Poramat Wongtawan <MTL91671@murata.com>
2024-02-05 15:58:40 +0700

    NEXT

---
+++ app/page.jsx
@@ -0,0 +1,24 @@
+import Feed from '@components/Feed'
+
+const Home = () => {
+    return (
+        <div>
+            <section className="w-full flex-center flex-col">
+                {/* <h1 className="head_text text-center">
+                    Discover & Share
+                    <br className="max-md:hidden" />
+                    <span className="orange_gradient text-center"> AI-Powered Prompts</span>
+                </h1>
+                <p className="desc text-center">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae,
+                    voluptatibus laudantium aspernatur sapiente quod modi quo excepturi dicta aliquam,
+                    facilis ipsa facere harum! Iste consectetur harum, qui laboriosam quis soluta?
+                </p> */}
+
+                {/* <Feed/> */}
+            </section>
+        </div>
+    )
+}
+
+export default Home
